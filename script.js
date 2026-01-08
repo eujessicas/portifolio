dad013fb28a280a7262bf0fe105f82f2ca204f03
Jessica <jjessicammayara@gmail.com>
2026-01-07 23:29:19 -0300

    versão2026

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const btnMenu = document.querySelector('.btn-abrir-menu');
-const menuMobile = document.querySelector('.menu-mobile');
-const overlay = document.querySelector('.overlay-menu');
-const btnFechar = document.querySelector('.menu-mobile .btn-fechar');
+const menuMobile = document.getElementById('menu-mobile');
+const btnFechar = document.querySelector('.btn-fechar');
+const overlay = document.getElementById('overlay-menu');
 
 // Abrir menu
 btnMenu.addEventListener('click', () => {
     menuMobile.classList.add('abrir-menu');
+    overlay.style.display = "block";
 });
 
-// Fechar menu
+// Fechar no X
 btnFechar.addEventListener('click', () => {
     menuMobile.classList.remove('abrir-menu');
+    overlay.style.display = "none";
 });
 
-// Fechar ao clicar no overlay
+// Fechar clicando fora
 overlay.addEventListener('click', () => {
     menuMobile.classList.remove('abrir-menu');
+    overlay.style.display = "none";
 });
-
